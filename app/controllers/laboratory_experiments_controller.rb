8e8b01c22704cd27e3552acb31629a5dd80239d7
rati jebisashvili <rati.jebisashvili.1@iliauni.edu.ge>
2022-12-16 18:27:42 +0400

    change test params

--- app/controllers/laboratory_experiments_controller.rb
+++ app/controllers/laboratory_experiments_controller.rb
@@ -18,7 +18,7 @@ class LaboratoryExperimentsController < ApplicationController
       plate_size: 96,
       samples: [['Sample-1', 'Sample-2', 'Sample-3', 'Sample-4', 'Sample-5', 'Sample-6', 'Sample-7', 'Sample-8', 'Sample-9'], ['Sample-1' ,'Sample-2', 'Sample-3'], ['Sample-4']],
       reagents: [['Pink', 'Yellow'],['Green'],['Violet']],
-      replicates: [2, 6, 5]
+      replicates: [2, 3, 1]
     }
     #
     # params.permit(:plate_size, samples: [], reagents: [], replicates: [])
